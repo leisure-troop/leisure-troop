f1a2624d80c1e0e25759523426880141e4465afe
lynda Sadoudi <lyndaironhack@gmail.com>
2025-05-07 13:18:59 +0200

    logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import free from '../assets/free.svg';
 function NavBar() {
 
     return (
         <div className="NavBar">
-            <div className="logo-image"><img className="logo" src="./src/assets/free.svg" /></div>
+            <div className="logo-image"><img src={free} alt="Free Logo" className="logo" /></div>
             <div className="navbar-text">  <h3>About us</h3> </div>
             <Link to="/create-trip" > <h3>Create Your Trip </h3> </Link>
 
